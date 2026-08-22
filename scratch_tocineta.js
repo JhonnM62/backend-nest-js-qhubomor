497e6ce4b3c0baac9c529d9e0977abc82a1fa72a
JhonnM62 <jonnymejia62@gmail.com>
2026-08-22 17:34:25 -0500

    chore: replace alitas recipe scratchpad with product debug script

--- scratch_tocineta.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const tocineta = await prisma.insumos.findFirst({
-    where: { nombre: { contains: 'Tocineta' } }
-  });
-  console.log("Tocineta Insumo:", tocineta);
-}
-main().finally(() => prisma.$disconnect());
